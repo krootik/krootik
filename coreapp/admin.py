dd2ae582b21ef833054944342602815d6fbdd720
krootik <yv.krutskiy@gmail.com>
2022-04-23 17:41:24 +0800

    hello

---
+++ coreapp/admin.py
@@ -0,0 +1,6 @@
+from gzip import READ
+from django.contrib import admin
+from coreapp.models import Restaurant
+
+admin.site.register(Restaurant)
+
--- foodtasker/settings.py
+++ foodtasker/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'coreapp',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+cloudinary.config (
+    cloud_name = "dsyoqvoy8",
+    api_key = "815179978712188",
+    api_secret = "6amXtiMeqAKRxwqSOUvWE9P6UtE"
+)
